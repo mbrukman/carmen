1849a5be9b7cfcecbccff18154b4cb27b786063c
Andrea del Rio <adelrio@gmail.com>
2017-06-01 11:18:04 -0700

    add removeWhiteSpace test to confirm whitespace has been removed

--- test/termops.removeWhiteSpace.test.js
+++ test/termops.removeWhiteSpace.test.js
@@ -1,38 +1,12 @@
 const tape = require('tape');
+const readline = require('readline');
+const fs = require('fs');
+const termops = require('../lib/util/termops');
 
-
-
-
-
-
-// confirm function removes whitespace
 tape('removeWhiteSpace', (assert) => {
-  assert.deepEqual(value1, value2, 'removeWhiteSpace() removes whitespace');
-  assert.end();
-});
-
-// What percentage of strings in each dataset end up overlapping with
-// others once normalized?
-tape('removeWhiteSpace', (assert) => {
-  assert.deepEqual(value1, value2, '% of overlapping strings after whitespace is removed.');
-  assert.end();
-});
-
-// What are the actual string values that do end up overlapping?
-//
-
-// Which ones overlap in a potentially positive way
-// (e.g. if Lake View Rd and Lakeview Rd collapse, that’s “positive” in
-//   that users may expect to find one when searching for the other)?
-//
-tape('removeWhiteSpace', (assert) => {
-  assert.deepEqual(value1, value2, '% of + overlapping strings after whitespace is removed');
-  assert.end();
-});
-
-
-// Which ones overlap in a potentially unexpected or negative way?
-tape('removeWhiteSpace', (assert) => {
-  assert.deepEqual(value1, value2, '% of -/undefined overlapping strings after whitespace is removed');
-  assert.end();
+    let noSpace = termops.removeWhiteSpace('this has a space');
+    let result = noSpace.indexOf(' ');
+    assert.deepEqual(noSpace, 'thishasaspace', 'result and removed spaces are the same')
+    assert.deepEqual(result, -1, 'removeWhiteSpace() removes whitespace');
+    assert.end();
 });
