28ccf307c3186ce0ef9a3deb2d7fc3fdcb85971b
Andrea del Rio <adelrio@gmail.com>
2017-06-01 13:52:16 -0700

    lowercase line

--- scripts/whitespace-analysis.js
+++ scripts/whitespace-analysis.js
@@ -1,7 +1,9 @@
-let fs = require('fs');
-let split = require('split');
-let stream = require('stream');
-let termops = require('../lib/util/termops.js');
+#!/usr/bin/env node
+
+const fs = require('fs');
+const split = require('split');
+const stream = require('stream');
+const termops = require('../lib/util/termops.js');
 
 // 1. Add a basic no-op CLI command scripts/whitespace-analysis.js that
 // can handle an input stream and output the input without any changes
@@ -19,17 +21,12 @@ let termops = require('../lib/util/termops.js');
 // Springfield Town  => springfieldtown
 
 
-//
-// you may want to continue to read from `process.stdin`
-// by using stdin you can let `cat` do the file reading for you
-//
-// ```cat <any file> | <your command>
-// ```
-
 process.stdin
     .pipe(split())
     .on('data', function(line) {
-        console.log(termops.removeWhiteSpace(line));
+      let result = line.toLowerCase();
+      result = termops.removeWhiteSpace(result)
+        console.log(result);
     });
 
 
