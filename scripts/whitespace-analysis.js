bf70561eb9f775526f522a9589c05c71f9820cf3
Andrea del Rio <adelrio@gmail.com>
2017-06-01 17:37:33 -0700

    remove excess

--- scripts/whitespace-analysis.js
+++ scripts/whitespace-analysis.js
@@ -5,33 +5,7 @@ const split = require('split');
 const stream = require('stream');
 const termops = require('../lib/util/termops.js');
 
-// 1. Add a basic no-op CLI command scripts/whitespace-analysis.js that
-// can handle an input stream and output the input without any changes
-//
-// eg. cat us-place-text-uniq.txt | ./scripts/whitespace-analysis.js
-//
-// 2. Modify the script to lowercase and call the whitespace remover
-// utility function on each incoming line
-//
-// 3. Adjust the script to store a mapping between output and input strings
-// and output the results in a format you can use to answer the questions above.
-
-// # Initial script just normalizes output
-// Spring Field Town => springfieldtown
-// Springfield Town  => springfieldtown
-
-// # Next pass lets you go backwards from result to possible input values
-// springfieldtown => ["Spring Field Town", "Springfield Town"]
-
-
-// What percentage of strings in each dataset end up overlapping with others
-// once normalized?
-//
-// What are the actual string values that do end up overlapping?
-// Which ones overlap in a potentially positive way
-// (e.g. if Lake View Rd and Lakeview Rd collapse, that’s “positive” in
-//   that users may expect to find one when searching for the other)?
-// Which ones overlap in a potentially unexpected or negative way?
+// see ticket: https://github.com/mapbox/carmen/issues/632
 
 let total = 0;
 let hash = {};
@@ -39,31 +13,37 @@ let result = 0;
 let sameAmt = 0;
 let array = [];
 
+const transmuteData = (line) => {
+    // normalize line
+    let resultLine = line.toLowerCase();
+    resultLine = termops.removeWhiteSpace(resultLine);
+    // track total lines
+    total++;
+    // track # of times the line is the same as other queries
+    if (hash[resultLine] !== undefined) {
+        sameAmt++;
+    } else {
+        hash[resultLine] = [];
+    }
+    hash[resultLine].push(line);
+};
+
+
+// In order to retrieve results run
+// `cat filename | ./scripts/whitespace-analysis.js  > results2.json`
+// in terminal
 process.stdin
   .pipe(split())
   .on('data', (line) => {
-    // normalize the lien
-      let resultLine = line.toLowerCase();
-      resultLine = termops.removeWhiteSpace(resultLine);
-      // track total lines
-      total++;
-      // track # of times the line is the same
-      if (hash[resultLine] !== undefined) {
-          sameAmt++;
-      } else {
-          hash[resultLine] = [];
-      }
-      hash[resultLine].push(line);
+      transmuteData(line);
   })
   .on('end', () => {
-      console.log(hash);
-
       Object.keys(hash).forEach((el) => {
           if (hash[el].length > 1) {
               array.push(hash[el]);
           }
       });
-      console.log(array);
       result = sameAmt/total;
-      console.log(result);
+      // console.log(result);
+      // console.log(JSON.stringify(array, null, 2));
   });
